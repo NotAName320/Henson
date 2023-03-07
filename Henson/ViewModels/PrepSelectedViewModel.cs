37b311f3c318ed7e62f4258c2a433672a2434c1b
Ray Chen <rc144@outlook.com>
2023-03-06 22:43:15 -0800

    hey buttons kinda work now

--- Henson/ViewModels/PrepSelectedViewModel.cs
+++ Henson/ViewModels/PrepSelectedViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using Henson.Models;
+using MessageBox.Avalonia.DTO;
+using MessageBox.Avalonia.Enums;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Henson.ViewModels
 {
@@ -12,20 +16,87 @@ namespace Henson.ViewModels
     {
         private NsClient Client { get; }
         private List<NationLoginViewModel> Nations { get; set; }
+        private int LoginIndex { get; set; } = 0;
+        private string CurrentChk { get; set; } = "";
+        public ICommand ActionButtonCommand { get; }
+        public Interaction<MessageBoxViewModel, ButtonResult> MessageBoxDialog { get; } = new();
+
 
         public PrepSelectedViewModel(List<NationLoginViewModel> nations, NsClient client)
         {
             Nations = nations;
             Client = client;
+
+            ActionButtonCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                if (LoginIndex == Nations.Count)
+                {
+                    MessageBoxViewModel dialog = new(new MessageBoxStandardParams
+                    {
+                        ContentTitle = "Logins complete",
+                        ContentMessage = $"All nations have been prepped. Please close the window now.",
+                        Icon = Icon.Error,
+                    });
+                    await MessageBoxDialog.Handle(dialog);
+                    return;
+                }
+
+                NationLoginViewModel currentNation = Nations[LoginIndex];
+
+                switch(buttonText)
+                {
+                    case "Login":
+                        var result = Client.Login(currentNation);
+                        if(result != null)
+                        {
+                            CurrentChk = result;
+                            CurrentLogin = currentNation.Name;
+
+                            ButtonText = AlsoApplyWA ? "Apply WA" : "Move to JP";
+                        }
+                        else
+                        {
+                            MessageBoxViewModel dialog = new(new MessageBoxStandardParams
+                            {
+                                ContentTitle = "Login Failed",
+                                ContentMessage = $"Logging in to {currentNation.Name} failed. Going to the next nation.",
+                                Icon = Icon.Error,
+                            });
+                            await MessageBoxDialog.Handle(dialog);
+                            LoginIndex++;
+                        }
+                        break;
+                }
+            });
         }
 
         private string buttonText = "Login";
         public string ButtonText
         {
-            get { return buttonText; }
+            get => buttonText;
             set
             {
                 this.RaiseAndSetIfChanged(ref buttonText, value);
+            }
+        }
+
+        private bool alsoApplyWA = true;
+        public bool AlsoApplyWA
+        {
+            get => alsoApplyWA;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref alsoApplyWA, value);
+            }
+        }
+
+        private string currentLogin = "";
+        public string CurrentLogin
+        {
+            get => currentLogin;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref currentLogin, value);
             }
         }
     }
--- Henson/Views/PrepSelectedWindow.axaml.cs
+++ Henson/Views/PrepSelectedWindow.axaml.cs
@@ -1,5 +1,15 @@
+using Avalonia.Controls;
+using Avalonia.Platform;
+using Avalonia;
 using Avalonia.ReactiveUI;
 using Henson.ViewModels;
+using MessageBox.Avalonia.Enums;
+using ReactiveUI;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System;
+using Avalonia.Media.Imaging;
 
 namespace Henson.Views
 {
@@ -8,6 +18,20 @@ namespace Henson.Views
         public PrepSelectedWindow()
         {
             InitializeComponent();
+            this.WhenActivated(d => d(ViewModel!.MessageBoxDialog.RegisterHandler(ShowMessageBoxDialog)));
+        }
+
+        private async Task ShowMessageBoxDialog(InteractionContext<MessageBoxViewModel, ButtonResult> interaction)
+        {
+            var parameters = interaction.Input.Params;
+
+            var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+            parameters.WindowIcon = new WindowIcon(new Bitmap(assets!.Open(new Uri("avares://Henson/Assets/henson-icon.ico"))));
+            parameters.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            var messageBox = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow(interaction.Input.Params);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) SystemSounds.Beep.Play();
+            interaction.SetOutput(await messageBox.ShowDialog(this));
         }
     }
 }
