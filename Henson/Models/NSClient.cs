81d073c00d443a0f5b404082f231325091574652
Ray Chen <rc144@outlook.com>
2023-07-05 14:40:36 -0400

    add license header

--- Henson/Models/NSClient.cs
+++ Henson/Models/NSClient.cs
@@ -27,7 +27,6 @@ using ReactiveUI;
 using RestSharp;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
