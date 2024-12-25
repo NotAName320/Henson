43993c546f624cfedb838b3639b240e7fd237db9
Ray Chen <rc144@outlook.com>
2024-12-25 08:31:43 -0500

    add some license comments

--- Henson/MessageBox/CustomMsBox.cs
+++ Henson/MessageBox/CustomMsBox.cs
@@ -1,4 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿/*
+Extended message box Controller
+Copyright (C) 2024 NotAName320
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using MsBox.Avalonia.Base;
 
@@ -6,6 +25,7 @@ namespace Henson.MessageBox;
 
 public class CustomMsBox<V, VM, T> : IMsBox<T> where V : UserControl, IFullApi<T>, ISetCloseAction where VM : ISetFullApi<T>, IInput
 {
+    // im not fucking documenting this shit. gl
     private readonly V _view;
     private readonly VM _viewModel;
     private readonly Styling _styling;
